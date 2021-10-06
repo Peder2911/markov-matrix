1875955cfa4e524088202781b0ba4176bfa80a5b
peder2911 <pglandsverk@gmail.com>
2021-10-06 12:52:46 +0200

    Added proper tests

--- tests/test_matrix_chain.py
+++ tests/test_matrix_chain.py
@@ -4,12 +4,25 @@ import numpy as np
 from unittest import TestCase
 from markov_matrix import matrix_chain
 
+np.random.seed(1337)
+
 class TestMatrixChain(TestCase):
     def test_matrix_chain(self):
         chain = matrix_chain(np.array([
                 [ .8 , .2 , .0 ,],
                 [ .1 , .8 , .1 ,],
                 [ .0 ,1.0 , .0 ,],
-            ]))
-        draws = islice(chain,100)
-        print([*draws][-1])
+            ]), shape=(3,3))
+        draws = islice(chain,25)
+        for mat in draws:
+            self.assertEqual(mat.shape, (3,3))
+    def test_distrib(self):
+        chain = matrix_chain(np.array([
+                [ .5 , .5 ],
+                [ .5 , .5 ],
+            ]), shape=(3,3))
+        draws = np.stack([*islice(chain,1000)],axis=2)
+        means = draws.mean(axis=2)
+        for number in means.ravel():
+            diff = abs(.5 - number)
+            self.assertLess(diff, .15)
